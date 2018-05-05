aaf58ab79a096ac1170a41c423ec35e1b3533a70
Roman <roman.politylo777@gmail.com>
2018-05-05 18:02:56 -0400

    My first repository

---
+++ dmytro1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    char character;
+    printf("Enter single character: ");
+    scanf("%c", &character);
+
+    // printf("Char: %c, int: %d\n", character, character);
+
+    if (character >= 'a' && character <= 'z'){
+        printf("This is mall letter.\n");
+    }
+    else if (character >= 'A' && character <= 'Z'){
+        printf("This is big letter.\n");
+    }
+    else if (character >= '0' && character <= '9'){
+        printf("This is number.\n");
+    }
+}
